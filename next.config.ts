70e01c10da302535aa463beb5f2518520bd4ae06
Eng_SabryHosny <sabry3hosny666@gmail.com>
2025-05-24 15:56:58 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Remove the 'output: export' line
   images: {
     unoptimized: true,
   },
-  // This ensures that static assets like images are properly included in the build
-  distDir: 'out',
   // Disable trailing slashes in URLs
   trailingSlash: false,
 };
